5f352ae712ee1279804377de25b9273ba589a757
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-12-23 13:56:26 -0300

    config express server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('batata')
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+app.use('/',
+  express.static(
+    resolve(
+      __dirname,
+      './build'
+    )
+  )
+)
+
+app
+  .listen(process.env.PORT || 3000, (err) => {
+    if (err) { return console.log(err) }
+
+    console.log('Tudo funcionando certinho')
+  })
